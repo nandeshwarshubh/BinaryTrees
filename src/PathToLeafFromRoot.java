a3e54c00fe33ae6a9dab83f1ab20302771bfc06f
Shubham <shubham.nandeshwar94@gmail.com>
2021-09-07 21:24:14 +0530

    Added path to leaf from root in range and transform to left cloned tree code

---
+++ src/PathToLeafFromRoot.java
@@ -0,0 +1,105 @@
+import java.util.Stack;
+
+public class PathToLeafFromRoot {
+
+    public static class Node {
+
+        Integer data;
+        Node left;
+        Node right;
+
+        Node(Integer data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static class Pair {
+            Node node;
+            int state;
+
+            public Pair(Node node, int state) {
+                this.node = node;
+                this.state = state;
+            }
+        }
+
+
+        //Print all paths whose sum is in range from low to high
+        public static void printPathToLeafFromRoot(Node node,
+                                                   String path,
+                                                   int sum,
+                                                   int low,
+                                                   int high) {
+            if (node == null) {
+                return;
+            }
+            if (node.left == null && node.right == null) {
+                sum += node.data;
+                if (sum >= low && sum <= high) {
+                    System.out.println(path + " " + node.data + "");
+                }
+                return;
+            }
+            printPathToLeafFromRoot(node.left,
+                    path + " " + node.data + "",
+                    sum + node.data,
+                    low,
+                    high);
+            printPathToLeafFromRoot(node.right,
+                    path + " " + node.data + "",
+                    sum + node.data,
+                    low,
+                    high);
+        }
+
+
+        /*
+         * 1-left
+         * 2-right
+         * 3-pop
+         * */
+        public static void main(String[] args) {
+            Integer[] arr = {50, 25, 12, null, null,
+                    37, 30, null, null, null,
+                    75, 62, null, 70, null,
+                    null, 87, null, null};
+
+            Stack<Pair> st = new Stack<>();
+            Node root = new Node(arr[0], null, null);
+
+            Pair rp = new Pair(root, 1);
+            st.push(rp);
+            int idx = 0;
+            while (st.size() > 0) {
+                Pair top = st.peek();
+                if (top.state == 1) {
+                    idx++;
+                    if (arr[idx] != null) {
+                        top.node.left = new Node(arr[idx], null, null);
+                        Pair leftPair = new Pair(top.node.left, 1);
+                        st.push(leftPair);
+                    } else {
+                        top.node.left = null;
+                    }
+                    top.state++;
+                } else if (top.state == 2) {
+                    idx++;
+                    if (arr[idx] != null) {
+                        top.node.right = new Node(arr[idx], null, null);
+
+                        Pair rightPair = new Pair(top.node.right, 1);
+                        st.push(rightPair);
+                    } else {
+                        top.node.right = null;
+                    }
+                    top.state++;
+                } else {
+                    st.pop();
+                }
+            }
+
+            printPathToLeafFromRoot(root, "", 0, 70, 213);
+        }
+    }
+}
---
+++ src/TransformToLeftClonedTree.java
@@ -0,0 +1,95 @@
+import java.util.Stack;
+
+public class TransformToLeftClonedTree {
+}
+
+
+import java.util.Stack;
+
+public class PathToLeafFromRoot {
+
+    public static class Node {
+
+        Integer data;
+        Node left;
+        Node right;
+
+        Node(Integer data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static class Pair {
+            Node node;
+            int state;
+
+            public Pair(Node node, int state) {
+                this.node = node;
+                this.state = state;
+            }
+        }
+
+        public static Node transformToLeftCloned(Node node) {
+            if (node == null) {
+                return null;
+            }
+            Node leftClonedRoot = transformToLeftCloned(node.left);
+            Node rightClonedRoot = transformToLeftCloned(node.right);
+
+            Node newLeft = new Node(node.data, leftClonedRoot, null);
+            node.left = newLeft;
+            node.right = rightClonedRoot;
+            return node;
+        }
+
+
+        /*
+         * 1-left
+         * 2-right
+         * 3-pop
+         * */
+        public static void main(String[] args) {
+            Integer[] arr = {50, 25, 12, null, null,
+                    37, 30, null, null, null,
+                    75, 62, null, 70, null,
+                    null, 87, null, null};
+
+            Stack<Pair> st = new Stack<>();
+            Node root = new Node(arr[0], null, null);
+
+            Pair rp = new Pair(root, 1);
+            st.push(rp);
+            int idx = 0;
+            while (st.size() > 0) {
+                Pair top = st.peek();
+                if (top.state == 1) {
+                    idx++;
+                    if (arr[idx] != null) {
+                        top.node.left = new Node(arr[idx], null, null);
+                        Pair leftPair = new Pair(top.node.left, 1);
+                        st.push(leftPair);
+                    } else {
+                        top.node.left = null;
+                    }
+                    top.state++;
+                } else if (top.state == 2) {
+                    idx++;
+                    if (arr[idx] != null) {
+                        top.node.right = new Node(arr[idx], null, null);
+
+                        Pair rightPair = new Pair(top.node.right, 1);
+                        st.push(rightPair);
+                    } else {
+                        top.node.right = null;
+                    }
+                    top.state++;
+                } else {
+                    st.pop();
+                }
+            }
+
+
+        }
+    }
+}
